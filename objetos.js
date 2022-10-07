d227a4979a3a1b9597cad87fdec4763e927df053
FernandoLepore <juliofernandolepore@gmail.com>
2022-10-07 19:11:58 -0300

    objetos en ts

---
+++ objetos.js
@@ -0,0 +1,8 @@
+"use strict";
+// practicando y aprendiendo con OBJETOS
+let persona = {
+    nombre: "fernando",
+    apellido: "lepore",
+    edad: 39,
+    mascotas: 2,
+};
---
+++ objetos.ts
@@ -0,0 +1,8 @@
+// practicando y aprendiendo con OBJETOS
+
+let persona: { nombre: string, apellido: string, edad: number, mascotas: number } = {
+    nombre: "fernando",
+    apellido: "lepore",
+    edad: 39,
+    mascotas: 2,
+}
