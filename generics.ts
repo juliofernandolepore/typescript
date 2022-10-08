33b2e391ef56c7b378b085e77683918146f8c56a
FernandoLepore <juliofernandolepore@gmail.com>
2022-10-08 11:36:29 -0300

    generics

---
+++ generics.ts
@@ -0,0 +1,9 @@
+//Genericos o generics
+
+function saludar<T>():any{ return "hola"}
+
+/* la <T> es un tipo generico que declara que va a ser cualquier tipo, por
+convencion se usa la letra T, y tiene el efecto de que se fije un tipo de 
+dato para despues que no se le apliquen metodos de otro tipo */
+console.log(saludar().floor)
+console.log(saludar().lenght)
