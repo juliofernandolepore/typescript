ce68b2c67b5dc9d6d736d0fefb9334b9d48384f8
Fernando lepore vm <vm_cercatore@hotmail.com>
2022-11-06 17:09:10 -0300

    generics

--- generics.js
+++ generics.js
@@ -1,8 +1,12 @@
 "use strict";
-//Genericos o generics
+//Genericos o generics CON TYPESCRIPT
 function saludar() { return "hola"; }
 /* la <T> es un tipo generico que declara que va a ser cualquier tipo, por
 convencion se usa la letra T, y tiene el efecto de que se fije un tipo de
 dato para despues que no se le apliquen metodos de otro tipo */
 console.log(saludar().floor);
 console.log(saludar().lenght);
+/* DECLARO UN ARREGLO QUE SOLO VA A ACEPTAR NUMEROS */
+var arrayNumeros;
+arrayNumeros = [23, 70, 45, 30, 70, 4, 60];
+console.log(arrayNumeros[2]);
